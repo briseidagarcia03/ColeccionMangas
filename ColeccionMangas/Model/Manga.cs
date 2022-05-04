432199420ff7ede9831d480ab9481800593cd386
brise <briseidagarcia033@gmail.com>
2022-05-03 21:37:33 -0500

    Agregar archivos de proyecto.

---
+++ ColeccionMangas/Model/Manga.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangasMVVM.Model
+{
+    public class Manga
+    {
+        public string Titulo { get; set; } = "";
+        public string Genero { get; set; } = "";
+        public string Autor { get; set; } = "";
+        public int Anio { get;  set; }
+        public int NumTomos { get; set; }
+        public string Sinopsis { get; set; } = "";
+        public string Imagen { get; set; } = "";
+         
+
+    }
+}
